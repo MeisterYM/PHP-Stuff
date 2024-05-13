4967400b00229c294fb584f9d0bdefae900094da
MeisterYM <yannick_mart@yahoo.com>
2024-05-13 16:32:27 -0500

    First commit

---
+++ Math.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Math</title>
+    </head>
+    <body>
+        <?php
+            $number = 1.0;
+
+            if($number == 1){
+                echo "You're number one!";
+            } else {
+                echo "Uhh, it's okay...";
+            }
+        ?>
+    </body>
+</html>
